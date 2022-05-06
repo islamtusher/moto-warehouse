2240c2334ae6fbe87bdc9f4c9d37cbf5eedc8d85
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-05-07 00:24:22 +0600

    load bikes data from db

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Collection.css'
+
 const Collection = () => {
+    const [bikes, setBikes] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/bikes')
+            .then(res => res.json())
+            .then(data => setBikes(data))
+        
+    }, [])
+    console.log(bikes);
     return (
         <div className='collecton-area'>
-            <h1>collecton</h1>
+            <h1>collecton {bikes?.length}</h1>
         </div>
     );
 };
